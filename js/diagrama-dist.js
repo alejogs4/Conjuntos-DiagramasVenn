e09e5ade8e619630198dff08bb09486c6830275e
alejogs4 <alejogs4@gmail.com>
2017-09-28 13:25:18 -0500

    4.1

--- js/diagrama-dist.js
+++ js/diagrama-dist.js
@@ -1,21 +1,51 @@
 "use strict";
 
-var U = 0, A = 1, B = 2, C = 3, conjuntoA = [], conjuntoB = [], conjuntoC = [], chart = venn.VennDiagram().width(500).height(500), diagrama = d3.select("#venn");
+var U = 0, A = 1, B = 2, C = 3, AB = 4, ABC = 6, BC = 5;
+
+console.log(sets);
+
+var conjuntoA = [], conjuntoB = [], conjuntoC = [], conjuntoATemporal = [], conjuntoBTemporal = [], conjuntoCTemporal = [], chart = venn.VennDiagram().width(500).height(500), diagrama = d3.select("#venn");
 
 diagrama.datum(sets).call(chart);
 
-var aInput = document.getElementById("a-element"), bInput = document.getElementById("b-element"), cInput = document.getElementById("c-element"), updateGroup = function(t, e) {
-    sets[t].size = e.value.length, sets[U].size = sets[A].size + sets[B].size + sets[C].size;
-    var n = e.value.split("");
-    t == A ? n.forEach(function(t) {
-        conjuntoA.push(t);
-    }, void 0) : t == B ? n.forEach(function(t) {
-        conjuntoB.push(t);
-    }, void 0) : n.forEach(function(t) {
-        conjuntoC.push(t);
-    }, void 0), filterGroups(conjuntoA, conjuntoB, conjuntoC);
-}, filterGroups = function(t, e, n) {
-    var o = [];
+var ejecutarButton = document.getElementById("ejecutar"), aInput = document.getElementById("a-element"), bInput = document.getElementById("b-element"), cInput = document.getElementById("c-element"), updateGroup = function() {
+    "" == aInput.value || "" == bInput.value || "" == cInput.value ? alert("Todos los campos deben estar llenos") : (conjuntoA = aInput.value.split(""), 
+    conjuntoATemporal = aInput.value.split(""), conjuntoB = bInput.value.split(""), 
+    conjuntoBTemporal = bInput.value.split(""), conjuntoC = cInput.value.split(""), 
+    conjuntoCTemporal = cInput.value.split(""), filterGroups(conjuntoA, conjuntoB, conjuntoC));
+}, filterGroups = function(t, e, o) {
+    var n = [], l = [], s = [], a = [];
+    n.length = 0, l.length = 0, s.length = 0, a.length = 0, filterArrays(t, e, n);
+    var i = n.filter(function(t, e) {
+        return n.indexOf(t) == e;
+    });
+    filterNot(i, t), filterNot(i, e), filterArrays(i, o, a);
+    var r = a.filter(function(t, e) {
+        return a.indexOf(t) == e;
+    });
+    filterNot(r, i), filterNot(r, o), filterNot(r, t), filterNot(r, e), filterArrays(e, o, s);
+    var u = s.filter(function(t, e) {
+        return s.indexOf(t) == e;
+    });
+    filterNot(u, e), filterNot(u, o), filterArrays(o, t, l);
+    var c = l.filter(function(t, e) {
+        return l.indexOf(t) == e;
+    });
+    filterNot(c, e), filterNot(c, t), console.log(i), console.log(t), console.log(e), 
+    console.log(o), console.log(r), console.log(u), sets[A].label = t.join(","), sets[A].size = t.length + 10, 
+    sets[B].label = e.join(","), sets[B].size = e.length + 9, sets[C].label = o.join(","), 
+    sets[C].size = o.length + 8, sets[AB].label = i.join(","), sets[AB].size = i.length + 5, 
+    sets[ABC].label = r.join(","), sets[ABC].size = r.length + 5, sets[BC].label = u.join(","), 
+    sets[BC].size = u.length + 5, sets[7].label = c.join(","), sets[7].size = c.length + 5, 
+    diagrama.datum(sets).call(chart);
+}, filterNot = function(t, e) {
+    t.forEach(function(t) {
+        for (var o = 0; o < e.length; o++) if (t == e[o]) {
+            e.splice(o, 1);
+            break;
+        }
+    });
+}, filterArrays = function(t, e, o) {
     t.forEach(function(t) {
         for (var n = 0; n < e.length; n++) if (t == e[n]) {
             o.push(t);
@@ -24,12 +54,8 @@ var aInput = document.getElementById("a-element"), bInput = document.getElementB
     });
 };
 
-aInput.addEventListener("change", function() {
-    updateGroup(A, this);
-}), bInput.addEventListener("change", function() {
-    updateGroup(B, this);
-}), cInput.addEventListener("change", function() {
-    updateGroup(C, this);
+ejecutarButton.addEventListener("click", function() {
+    updateGroup();
 });
 
 var tooltip = d3.select("body").append("div").attr("class", "venntooltip");
--- js/diagrama.js
+++ js/diagrama.js
@@ -1,52 +1,148 @@
-const U=0;
-const A=1;
-const B=2;
-const C=3;
+const U = 0;
+const A = 1;
+const B = 2;
+const C = 3;
+const AB = 4;
+const ABC = 6;
+const BC = 5;
 
-let conjuntoA=[];
-let conjuntoB=[];
-let conjuntoC=[];
+console.log(sets);
+let conjuntoA = [];
+let conjuntoB = [];
+let conjuntoC = [];
+
+let conjuntoATemporal = [];
+let conjuntoBTemporal = [];
+let conjuntoCTemporal = [];
+
+
 
 let chart = venn.VennDiagram()
-.width(500)
-.height(500);
+    .width(500)
+    .height(500);
 
 var diagrama = d3.select("#venn")
 diagrama.datum(sets).call(chart);
 
-let aInput=document.getElementById('a-element');
-let bInput=document.getElementById('b-element');
-let cInput=document.getElementById('c-element');
+let ejecutarButton = document.getElementById('ejecutar');
+let aInput = document.getElementById('a-element');
+let bInput = document.getElementById('b-element');
+let cInput = document.getElementById('c-element');
 
-const updateGroup=(group,ob)=>{
-    sets[group].size=ob.value.length;
-    sets[U].size=sets[A].size+sets[B].size+sets[C].size;
-    let labelGroup=ob.value.split("");
-
-    if(group==A){
-        labelGroup.forEach(function(el) {
-            conjuntoA.push(el);
-        }, this);
-    }else if(group==B){
-        labelGroup.forEach(function(el){
-            conjuntoB.push(el);
-        },this);
-    }else{
-        labelGroup.forEach(function(el){
-            conjuntoC.push(el);
-        },this);
+const updateGroup = () => {
+    if (aInput.value == "" || bInput.value == "" || cInput.value == "") {
+        alert("Todos los campos deben estar llenos");
     }
-    filterGroups(conjuntoA,conjuntoB,conjuntoC);
+    else {
+        conjuntoA = aInput.value.split('');
+        conjuntoATemporal = aInput.value.split('');
+
+        conjuntoB = bInput.value.split('');
+        conjuntoBTemporal = bInput.value.split('');
+
+        conjuntoC = cInput.value.split('');
+        conjuntoCTemporal = cInput.value.split('');
+
+        filterGroups(conjuntoA, conjuntoB, conjuntoC);
+    }
     // sets[group].label=ob.value;
     // diagrama.datum(sets).call(chart);
 }
 
-const filterGroups=(groupA,groupB,groupC)=>{
-    let intersectionGroupAAndGroupB=[];
-    groupA.forEach(function(el){
-        for(let i=0;i<groupB.length;i++){
-            if(el==groupB[i]){
-                intersectionGroupAAndGroupB.push(el);
+const filterGroups = (groupA, groupB, groupC) => {
+    let intersectionGroupAAndGroupB = [];
+    let intersectionGroupAAndGroupC = [];
+    let intersectionGroupBAndGroupC = [];
+    let intersectionABC = [];
+
+    intersectionGroupAAndGroupB.length = 0;
+    intersectionGroupAAndGroupC.length = 0;
+    intersectionGroupBAndGroupC.length = 0;
+    intersectionABC.length = 0;
+
+    filterArrays(groupA, groupB, intersectionGroupAAndGroupB);
+
+    //Filtro elementos repetidos
+    let filterArrayAB = intersectionGroupAAndGroupB.filter((el, pos) => {
+        return intersectionGroupAAndGroupB.indexOf(el) == pos;
+    });
+
+    filterNot(filterArrayAB, groupA);
+    filterNot(filterArrayAB, groupB);
+
+    filterArrays(filterArrayAB, groupC, intersectionABC);
+
+    let filterABC = intersectionABC.filter((el, pos) => {
+        return intersectionABC.indexOf(el) == pos;
+    });
+    filterNot(filterABC, filterArrayAB);
+    filterNot(filterABC, groupC);
+    filterNot(filterABC, groupA);
+    filterNot(filterABC, groupB);
+
+    filterArrays(groupB, groupC, intersectionGroupBAndGroupC);
+
+    let filterArrayBC = intersectionGroupBAndGroupC.filter((el, pos) => {
+        return intersectionGroupBAndGroupC.indexOf(el) == pos;
+    });
+    filterNot(filterArrayBC, groupB);
+    filterNot(filterArrayBC, groupC);
+
+
+    filterArrays(groupC, groupA, intersectionGroupAAndGroupC);
+
+    let filterArrayAC = intersectionGroupAAndGroupC.filter((el, pos) => {
+        return intersectionGroupAAndGroupC.indexOf(el) == pos;
+    });
+    filterNot(filterArrayAC, groupB);
+    filterNot(filterArrayAC, groupA);
+
+    console.log(filterArrayAB);
+    console.log(groupA);
+    console.log(groupB);
+    console.log(groupC);
+    console.log(filterABC);
+    console.log(filterArrayBC);
+
+    sets[A].label = groupA.join(',');
+    sets[A].size = groupA.length + 10;
+
+    sets[B].label = groupB.join(',');
+    sets[B].size = groupB.length + 9;
+
+    sets[C].label = groupC.join(',');
+    sets[C].size = groupC.length + 8;
+
+    sets[AB].label = filterArrayAB.join(',');
+    sets[AB].size = filterArrayAB.length + 5;
+
+    sets[ABC].label = filterABC.join(',');
+    sets[ABC].size = filterABC.length + 5;
+
+    sets[BC].label = filterArrayBC.join(',');
+    sets[BC].size = filterArrayBC.length + 5;
+
+    sets[7].label = filterArrayAC.join(',');
+    sets[7].size = filterArrayAC.length + 5;
+    diagrama.datum(sets).call(chart);
+}
+
+const filterNot = (intersection, group) => {
+    intersection.forEach((el) => {
+        for (let j = 0; j < group.length; j++) {
+            if (el == group[j]) {
+                group.splice(j, 1);
+                break;
+            }
+        }
+    });
+}
+
+const filterArrays = (main, reference, newA) => {
+    main.forEach((el) => {
+        for (let i = 0; i < reference.length; i++) {
+            if (el == reference[i]) {
+                newA.push(el);
                 break;
             }
         }
@@ -54,14 +150,8 @@ const filterGroups=(groupA,groupB,groupC)=>{
 }
 
 // Functions that update every group
-aInput.addEventListener('change',function(){
-    updateGroup(A,this);
-});
-bInput.addEventListener('change',function(){
-    updateGroup(B,this);
-});
-cInput.addEventListener('change',function(){
-    updateGroup(C,this);
+ejecutarButton.addEventListener('click', () => {
+    updateGroup();
 });
 
 
